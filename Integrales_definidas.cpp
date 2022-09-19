5c43e3a61463b010c97e07cfe68831ebf55f6b1d
Sturm0 <89245373+Sturm0@users.noreply.github.com>
2022-09-19 00:49:20 -0300

    Mejora en precisión y opción de límite inferior de integración

--- Integrales_definidas.cpp
+++ Integrales_definidas.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm> // es necesario para usar la función std::max_element
-// programa para calcular integrales definidas, por ahora el borde de integración inferior va a ser 0 y el superior un número positivo
-float funcion(float x) {
+// programa para calcular integrales definidas, por ahora el borde de integración inferior y superior se espera que sea un número positivo. O 0 en el caso del inferior
+double funcion(double x) {
 	return std::pow(x,2);
 }
 
 int main()
 {
-	float borde_superior = 5.0;
-	float paso = .001; // determina el espacio entre cada valor de "y" tomado
-	float tamaño_rectangulo = borde_superior/100; // determina la base del rectangulo
+	double borde_inferior = 5.0;
+	double borde_superior = 50.0;
+	double paso = .001; // determina el espacio entre cada valor de "y" tomado
+	double tamaño_rectangulo = (borde_superior-borde_inferior)/1000; // determina la base del rectangulo
 
 	int tam_rect_arreglo = tamaño_rectangulo/paso; // es el tamaño en posiciones del arreglo que tiene la base del rectangulo
-	int largo = borde_superior/paso;
-	float valores_funcion[largo];
-	float val_x = 0.0;
+	int largo = (borde_superior-borde_inferior)/paso;
+	double valores_funcion[largo];
+	double val_x = borde_inferior;
 
 	for (int i = 0; i<largo; i++) {
 		//este for se encarga de poblar la lista con valores de la función
@@ -24,11 +25,11 @@ int main()
 	}
 	
 
-	float resultado = 0;
+	double resultado = 0;
 	for (int i = tam_rect_arreglo; i<=largo; i += tam_rect_arreglo) {
 		
 		//acá se va a hacer la magia :D
-		float* mas_grande = std::max_element(&valores_funcion[i-tam_rect_arreglo],&valores_funcion[i]);
+		double* mas_grande = std::max_element(&valores_funcion[i-tam_rect_arreglo],&valores_funcion[i]);
 		resultado += *mas_grande * tamaño_rectangulo;
 		
 
